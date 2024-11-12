85567e0e8106fe9e95b8c16a364f265c1a30635c
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-11-12 21:46:28 +0700

    frist commit

---
+++ proses_data_upload_bc/main.py
@@ -0,0 +1,4 @@
+from read_proses import ReadProsesXlsx as rpx
+
+test  = rpx('./Madela_Remplate/10001.xlsx')
+
---
+++ proses_data_upload_bc/read_proses.py
@@ -0,0 +1,25 @@
+from spire.xls import *
+from spire.xls.common import *
+
+class ReadProsesXlsx:
+    def __init__(self,pathXlsx):
+        self.workbook = Workbook()
+        self.workbook.LoadFromFile(pathXlsx)
+        self.sheet = workbook.Worksheets[0]
+        self.sheetName = self.sheet.Name
+        self.fromulaList = []
+
+
+    def findFormula(self):
+        usedRange = self.sheet.AllocatedRange
+        for cell in usedRange:
+            if(cell.HasFormula):
+                cellName = cell.RangeAddressLocal
+                formula = cell.Formula
+                self.fromulaList.append(cellName + " = " + formula)
+
+
+    def endReadProses(self):
+        self.workbook.Dispose()
+
+
