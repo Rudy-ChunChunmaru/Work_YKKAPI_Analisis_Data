de121ff666825fe2d390e69e0510040da84198a9
rudymuinfo <rudymuinfo@gmail.com>
2024-11-13 07:14:54 +0000

    update program

--- proses_data_upload_bc/main.py
+++ proses_data_upload_bc/main.py
@@ -1,9 +1,16 @@
 from read_proses import ReadFormulaXlsx as rfx
 
+pathToGet = './Madela_Template/10001.xlsx'
+pathToSave = './Proses_Madela_Template/'
+xlsxName = ''
 
 
+prosesFormula = rfx(pathToGet=pathToGet,xlsxName=xlsxName)
+# prosesFormula.findFormula()
+# listFormula = prosesFormula.fromulaList
 
-test  = rfx('./Madela_Template/10001.xlsx')
-test.findFormula()
-print(test.fromulaList)
-test.endReadProses()
+# workBook = cwb(pathToGet=pathToGet,xlsxName=xlsxName)
+
+
+# Workbook.saveFile(pathToSave)
+# prosesFormula.endReadProses()
--- proses_data_upload_bc/read_proses.py
+++ proses_data_upload_bc/read_proses.py
@@ -2,13 +2,12 @@ from spire.xls import *
 from spire.xls.common import *
 
 class ReadFormulaXlsx:
-    def __init__(self,pathXlsx):
+    def __init__(self,pathToGet,xlsxName):
         self.workbook = Workbook()
-        self.workbook.LoadFromFile(pathXlsx)
+        self.workbook.LoadFromFile(pathToGet)
         self.sheet = self.workbook.Worksheets[0]
         self.sheetName = self.sheet.Name
         self.fromulaList = []
-
 
     def findFormula(self):
         usedRange = self.sheet.AllocatedRange
@@ -21,6 +20,31 @@ class ReadFormulaXlsx:
 
     def endReadProses(self):
         self.workbook.Dispose()
+
+
+class CopyWorkBook:
+    def __init__(self,pathToGet,xlsxName):
+        self.xlsxName = xlsxName
+        # old deworkbook
+        self.deWorkbook = Workbook()
+        self.deWorkbook.LoadFromFile(f'{pathToGet}{self.xlsxName}.xlsx')
+        self.deSheet = self.deWorkbook.Worksheets[0]
+        self.deSheetName = self.deSheet.Name
+        # new workbook
+        self.workbook = Workbook()
+        self.workbook.Worksheets.Add(desheetName)
+        self.workbook.CopyFrom(deSheet)
+        #close deworkbook
+        self.deWorkbook.Dispose()
+
+    def saveFile(self,pathToSave):
+        self.workbook.SaveToFile(f'{pathToSave}{self.xlsxName}.xlsx')
+        self.workbook.Dispose()
+
+
+
+
+
 
 
 
